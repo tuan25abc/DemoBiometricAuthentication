82140000376368bf9949df4c27692a378236ff78
Tuan Nguyen <tuan25abc@gmail.com>
2018-12-02 16:30:56 +0700

    Add ConfirmationProtectedPrompt feature

--- app/src/main/java/com/example/tuan25abc/demobiometricauthentication/MainActivity.kt
+++ app/src/main/java/com/example/tuan25abc/demobiometricauthentication/MainActivity.kt
@@ -6,6 +6,8 @@ import android.hardware.biometrics.BiometricPrompt
 import android.os.Build
 import android.os.Bundle
 import android.os.CancellationSignal
+import android.security.ConfirmationCallback
+import android.security.ConfirmationPrompt
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -30,6 +32,10 @@ class MainActivity : AppCompatActivity() {
                     && isFingerprintEnrolled(this)) {
                 displayBiometricPrompt(createAuthenticationCallback(rootView))
             }
+        }
+
+        button1.setOnClickListener {
+            displayConfirmationProtectedPrompt(createConfirmationCallback())
         }
     }
 
@@ -60,7 +66,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
                 super.onAuthenticationSucceeded(result)
-                Snackbar.make(rootView, "Success", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(rootView, "Authentication Succeeded", Snackbar.LENGTH_LONG).show()
             }
 
             override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
@@ -70,8 +76,45 @@ class MainActivity : AppCompatActivity() {
 
             override fun onAuthenticationFailed() {
                 super.onAuthenticationFailed()
-                Snackbar.make(rootView, "Failed", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(rootView, "Authentication Failed", Snackbar.LENGTH_LONG).show()
             }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.P)
+    private fun displayConfirmationProtectedPrompt(callback: ConfirmationCallback) {
+        val dialog = ConfirmationPrompt.Builder(this)
+                .setPromptText("Send $100 to Tuan?")
+                .setExtraData("Send to Tuan $100".toByteArray())
+                .build()
+        dialog.presentPrompt(this.mainExecutor, callback)
+    }
+
+    private fun createConfirmationCallback() = object : ConfirmationCallback() {
+        override fun onError(e: Throwable?) {
+            super.onError(e)
+            Snackbar.make(rootView, "Confirmation Error", Snackbar.LENGTH_LONG).show()
+        }
+
+        override fun onConfirmed(dataThatWasConfirmed: ByteArray) {
+            super.onConfirmed(dataThatWasConfirmed)
+            // Sign dataThatWasConfirmed using your generated signing key.
+            // By completing this process, you generate a "signed statement".
+            Snackbar.make(rootView, "Confirmation Confirmed: ${String(dataThatWasConfirmed)}", Snackbar.LENGTH_LONG).show()
+        }
+
+        override fun onDismissed() {
+            super.onDismissed()
+            // Handle case where user declined the prompt in the
+            // confirmation dialog.
+            Snackbar.make(rootView, "Confirmation Dismissed", Snackbar.LENGTH_LONG).show()
+        }
+
+        override fun onCanceled() {
+            super.onCanceled()
+            // Handle case where your app closed the dialog before the user
+            // could respond to the prompt.
+            Snackbar.make(rootView, "Confirmation Canceled", Snackbar.LENGTH_LONG).show()
         }
     }
 }
